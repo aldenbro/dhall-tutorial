c66bc7abe57ee2811a06bc303ab1d3270123e19c
David Aldenbro <david.aldenbro@gmail.com>
2025-09-21 11:26:10 +0200

    fix: change json import

--- dhall-scenario/setup.sh
+++ dhall-scenario/setup.sh
@@ -9,7 +9,3 @@ apt-get install -y dhall
 
 # Install dhall tools
 curl -L https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2 | tar xj --strip-components=1 -C /usr/local/bin
-
-# Give user the file
-mkdir -p /root/scenario
-cp /root/assets/info.json /root/scenario/
