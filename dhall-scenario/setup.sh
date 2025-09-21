9fb305836aa826f0b79177e1e365374d787b29de
David Aldenbro <david.aldenbro@gmail.com>
2025-09-21 10:44:42 +0200

    fix: correct setup script

--- dhall-scenario/setup.sh
+++ dhall-scenario/setup.sh
@@ -4,5 +4,8 @@ set -e
 # Update package list
 apt-get update -y
 
-# Install dhall and dhall-json
-apt-get install -y dhall dhall-json
+# Install dhall
+apt-get install -y dhall
+
+# Install dhall tools
+curl -L https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2 | tar xj --strip-components=1 -C /usr/local/bin
