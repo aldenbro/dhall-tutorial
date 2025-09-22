1b27302bdf06dfd39849ce1bb3791259e8ce499a
David Aldenbro <david.aldenbro@gmail.com>
2025-09-22 11:54:42 +0200

    fix: use explicit urls

--- dhall-scenario/step2/background.sh
+++ dhall-scenario/step2/background.sh
@@ -1,7 +1,5 @@
-GITHUB_PATH=https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/"
-
 cd /root
 
-wget "${GITHUB_PATH}User.dhall"
-wget "${GITHUB_PATH}Schema.dhall"
-curl -L "${GITHUB_PATH}info.json" -o "incorrect_info.json"
+wget "https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/User.dhall"
+wget "https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/Schema.dhall"
+curl -L "https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/info.json" -o "incorrect_info.json"
