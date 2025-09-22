64017b6f38c3863ce78775d77b67ebca1fd3a7c7
David Aldenbro <david.aldenbro@gmail.com>
2025-09-22 13:26:26 +0200

    fix: batcat install

--- dhall-scenario/setup.sh
+++ dhall-scenario/setup.sh
@@ -8,7 +8,7 @@ apt-get update -y
 apt-get install -y dhall
 
 # Install syntax highlighting alternative to cat
-sudo apt install bat
+apt-get install -y bat
 
 # Install dhall tools
 curl -L https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2 | tar xj --strip-components=1 -C /usr/local/bin
