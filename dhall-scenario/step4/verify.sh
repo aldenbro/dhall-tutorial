6b6ff4cd34cbbe1d19eab415c6482c7ea26a554f
David Aldenbro <david.aldenbro@gmail.com>
2025-09-23 16:13:04 +0200

    fix: step 4 verification

--- dhall-scenario/step4/verify.sh
+++ dhall-scenario/step4/verify.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-FILE_YAML="correct_info.yaml"
-FILE_CSV="correct_info.csv"
-LOCAL_YAML="/root/info.yaml"
-LOCAL_CSV="/root/info.csv"
-
 TMP_EXPECTED="$(mktemp)"
-curl -L "https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/${FILE_YAML}" -o "$TMP_EXPECTED"
+curl -L "https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/correct_info.csv" -o "$TMP_EXPECTED"
 
-if diff -wq "$LOCAL_YAML" "$TMP_EXPECTED" > /dev/null; then
-    rm -f "$TMP_EXPECTED"
-else
-    rm -f "$TMP_EXPECTED"
-    exit 1
-fi
-
-TMP_EXPECTED="$(mktemp)"
-curl -L "https://raw.githubusercontent.com/aldenbro/dhall-tutorial/refs/heads/main/dhall-scenario/assets/${FILE_CSV}" -o "$TMP_EXPECTED"
-
-if diff -wq "$LOCAL_CSV" "$TMP_EXPECTED" >/dev/null; then
+if diff -wq <(dhall text --file csv.dhall) "$TMP_EXPECTED" >/dev/null; then
     rm -f "$TMP_EXPECTED"
     exit 0
 else
