f0a8c7e8afee5c61ba568e0826338e699e374127
David Aldenbro <david.aldenbro@gmail.com>
2025-09-23 13:31:18 +0200

    fix: download order

--- dhall-scenario/setup.sh
+++ dhall-scenario/setup.sh
@@ -4,11 +4,11 @@ set -e
 # Update package list
 apt-get update -y
 
-# Install dhall
-apt-get install -y dhall
-
 # Install syntax highlighting alternative to cat
 apt-get install -y bat
+
+# Install dhall
+apt-get install -y dhall
 
 # Install dhall tools
 curl -L https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2 | tar xj --strip-components=1 -C /usr/local/bin
