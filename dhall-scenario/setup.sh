88e4cd0948653e347f4000f265a8c7ebff5bb8f4
David Aldenbro <david.aldenbro@gmail.com>
2025-09-23 13:24:55 +0200

    feat: add yaml

--- dhall-scenario/setup.sh
+++ dhall-scenario/setup.sh
@@ -12,3 +12,4 @@ apt-get install -y bat
 
 # Install dhall tools
 curl -L https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-json-1.7.12-x86_64-linux.tar.bz2 | tar xj --strip-components=1 -C /usr/local/bin
+curl -L https://github.com/dhall-lang/dhall-haskell/releases/download/1.42.2/dhall-yaml-1.7.12-x86_64-linux.tar.bz2 | tar xj --strip-components=1 -C /usr/local/bin
